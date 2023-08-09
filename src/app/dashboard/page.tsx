b8b5e5f7ea6f4a824d9b00a1a368c93ffcd4fd81
Gustavo Bastos <bastosgm@outlook.com>
2023-08-09 06:25:09 -0300

    feat: added db and methods to manipulate data

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,17 +5,98 @@ import { redirect } from "next/navigation";
 import { Textarea } from "@/components/textarea";
 import { FiShare2 } from "react-icons/fi";
 import { FaTrash } from "react-icons/fa";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
+
+import { db } from "@/services/firebaseConnection";
+import {
+  addDoc,
+  collection,
+  query,
+  orderBy,
+  where,
+  onSnapshot,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
+import Link from "next/link";
+
+interface TasksProps {
+  id: string;
+  created: Date;
+  public: boolean;
+  task: string;
+  user: string;
+}
 
 export default function Dashboard() {
   const { data: session, status } = useSession();
-  console.log(session);
 
-  if (!session?.user) redirect("/");
+  if (status === "unauthenticated") redirect("/");
 
   const [input, setInput] = useState("");
   const [isPublic, setIsPublic] = useState(false);
+  const [tasks, setTasks] = useState<TasksProps[]>([]);
+
+  useEffect(() => {
+    async function loadTasks() {
+      const tasksRef = collection(db, "tasks");
+      const q = query(
+        tasksRef,
+        orderBy("created", "desc"),
+        where("user", "==", session?.user?.email || ""),
+      );
+
+      onSnapshot(q, (snapshot) => {
+        let taskList: TasksProps[] = [];
+
+        snapshot.forEach((doc) => {
+          taskList.push({
+            id: doc.id,
+            task: doc.data().task,
+            created: doc.data().created,
+            user: doc.data().user,
+            public: doc.data().public,
+          });
+        });
+
+        setTasks(taskList);
+      });
+    }
+    loadTasks();
+  }, [session?.user?.email]);
+
+  async function handleRegisterTask(e: FormEvent) {
+    e.preventDefault();
+
+    if (!input) return;
+
+    try {
+      await addDoc(collection(db, "tasks"), {
+        task: input,
+        created: new Date(),
+        user: session?.user?.email,
+        public: isPublic,
+      });
+
+      setInput("");
+      setIsPublic(false);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async function handleShareTask(id: string) {
+    await navigator.clipboard.writeText(
+      `${process.env.NEXT_PUBLIC_URL}/task/${id}`,
+    );
+    alert("URL was successfully copied!");
+  }
+
+  async function handleDeleteTask(id: string) {
+    const docRef = doc(db, "tasks", id);
+    await deleteDoc(docRef);
+  }
 
   return (
     <div className={styles.container}>
@@ -23,7 +104,7 @@ export default function Dashboard() {
         <section className={styles.content}>
           <div className={styles.contentForm}>
             <h1 className={styles.title}>What is your next task now?</h1>
-            <form>
+            <form onSubmit={handleRegisterTask}>
               <Textarea
                 placeholder="Type your task here"
                 value={input}
@@ -52,20 +133,37 @@ export default function Dashboard() {
 
         <section className={styles.taskContainer}>
           <h2>My tasks</h2>
-          <article className={styles.task}>
-            <div className={styles.tagContainer}>
-              <label className={styles.tag}>PUBLIC</label>
-              <button className={styles.shareButton}>
-                <FiShare2 size={22} color="#3183ff" />
-              </button>
-            </div>
-            <div className={styles.taskContent}>
-              <p>My first simple of task!</p>
-              <button className={styles.trashButton}>
-                <FaTrash size={24} color="#ea3140" />
-              </button>
-            </div>
-          </article>
+          {tasks.map((task) => (
+            <article key={task.id} className={styles.task}>
+              {task.public && (
+                <div className={styles.tagContainer}>
+                  <label className={styles.tag}>PUBLIC</label>
+                  <button
+                    className={styles.shareButton}
+                    onClick={() => handleShareTask(task.id)}
+                  >
+                    <FiShare2 size={22} color="#3183ff" />
+                  </button>
+                </div>
+              )}
+
+              <div className={styles.taskContent}>
+                {task.public ? (
+                  <Link href={`/task/${task.id}`}>
+                    <p>{task.task}</p>
+                  </Link>
+                ) : (
+                  <p>{task.task}</p>
+                )}
+                <button
+                  className={styles.trashButton}
+                  onClick={() => handleDeleteTask(task.id)}
+                >
+                  <FaTrash size={24} color="#ea3140" />
+                </button>
+              </div>
+            </article>
+          ))}
         </section>
       </main>
     </div>
